dc9dde9213f10c56454e1a399eaaa286ff3403d7
Juaquin <Noahdavidian@hotmail.dk>
2017-09-18 17:03:13 +0200

    Made the Classes needed for the project!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is a test! Hello World!");
+        System.out.println("This is a test!");
     }
 }
