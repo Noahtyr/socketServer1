f9cd5ec5cc1d5ce8da7708f35f453a17ee433200
Juaquin <Noahdavidian@hotmail.dk>
2017-09-19 20:35:04 +0200

    Made some changes in ClientConnection Class. Creating the ClientConnection method, while throwing a socket exception!

--- src/com/company/clientConnection.java
+++ src/com/company/clientConnection.java
@@ -1,4 +1,22 @@
 package com.company;
 
-public class clientConnection {
+import	java.io.*;
+import	java.net.*;
+import	java.util.Scanner;
+
+public class clientConnection implements Runnable {
+
+   private Socket s;
+
+   public ClientConnection(Socket s) throws SocketException, IOException {
+
+       this.s=s;
+   }
+
+   @Override
+    public void run() {
+
+   }
+
+
 }
