b8c75e7ccb8c68961bb680e7c0d28eff43298fcc
Juaquin <Noahdavidian@hotmail.dk>
2017-09-20 09:45:05 +0200

    Made the Server.java class

--- src/com/company/server.java
+++ src/com/company/server.java
@@ -1,6 +1,66 @@
 package com.company;
+import	java.io.*;
+import	java.net.ServerSocket;
+import	java.net.Socket;
+import	java.util.Scanner;
 
 public class server {
+
+
+
+    public	class	Server {
+
+        public	static	void	main(String[]	args) {
+
+
+            try {
+
+                ServerSocket	ss	=	new	ServerSocket(8001);
+                System.out.println("Server	kører...");
+
+                while(true) {
+
+
+                    Socket	s	=	ss.accept();
+                    System.out.println("Klient	forbundet.");
+
+
+                    InputStream	input	=	s.getInputStream();
+                    OutputStream	output	=	s.getOutputStream();
+
+
+                    Scanner	in	=	new	Scanner(input);
+
+
+                    PrintWriter	out	=	new	PrintWriter(output,	true);
+
+
+                    out.println("Velkommen");
+
+
+                    boolean	done	=	false;
+                    while	(!done	&&	in.hasNextLine()) {
+
+                        String	stream	=	in.nextLine();
+                        if	(stream.equals("luk	ned")) {
+                            done	=	true;
+                        }
+                        else {
+
+                            out.println(stream);
+                        }
+                    }
+
+                    s.close();
+                    System.out.println("Connection to client closed...");
+                }
+            }
+            catch	(IOException	ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
 
 
 }
